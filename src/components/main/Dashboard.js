da41cd9689b25324bb721aab43863d77decfa4e4
seanchok <seanchok@gmail.com>
2019-11-27 23:50:27 +0800

    reorder table column fields

--- src/components/main/Dashboard.js
+++ src/components/main/Dashboard.js
@@ -28,8 +28,8 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const columns = [
-  { id: 'business_address', label: 'Business Address', minWidth: 150 },
   { id: 'business_name', label: 'Name', minWidth: 100 },
+  { id: 'business_address', label: 'Business Address', minWidth: 150 },
   { id: 'businesstype', label: 'Type', minWidth: 80 },
   { id: 'cash_balance', label: 'Cash Balance', minWidth: 50 },
   { id: 'owner_name', label: 'Owner', minWidth: 50 },
