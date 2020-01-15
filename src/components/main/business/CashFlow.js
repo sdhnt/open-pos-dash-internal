9bb0d8f7a288f09153bf1d522b55dd7a9cc3e69c
seanchok <seanchok@gmail.com>
2020-01-15 22:10:28 +0800

    build a table of net cash flow over period

--- src/components/main/business/CashFlow.js
+++ src/components/main/business/CashFlow.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Table from './Table';
 
 const CashFlow = props => {
   const { data } = props;
-  return <>cash flow</>;
+
+  return (
+    <>
+      <Typography variant={'h6'}>Net cash flow</Typography>
+      <Table
+        headers={[
+          {
+            key: 'period',
+            text: 'Period'
+          },
+          {
+            key: 'netCashFlow',
+            text: 'Net cash flow'
+          }
+        ]}
+        rows={data}
+      />
+    </>
+  );
 };
 
 export default CashFlow;
